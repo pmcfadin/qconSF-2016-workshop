a5c64dd99948054aea8d1c601fc08fe4e9d103cd
Patrick McFadin <pmcfadin@gmail.com>
2016-11-09 12:30:21 -0800

    Removed Target dir

--- webapp/target/classes/static-web/scripts/killrvideo.js
+++
@@ -1,7 +0,0 @@
-$("#querySelectorMenu li a").click(function(){
-    $("#querySelector:first-child").text($(this).text());
-    $("#querySelector:first-child").val($(this).value);
-    alert("blah");
-    //Stops the link doing what it would normally
-    return false;
-});
