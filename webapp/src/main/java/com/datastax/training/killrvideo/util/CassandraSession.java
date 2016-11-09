065090bfc28586c07938f6735b9d8ff6736fb214
Patrick McFadin <pmcfadin@gmail.com>
2016-11-09 15:46:13 -0800

    Changes for Exercise 1

--- webapp/src/main/java/com/datastax/training/killrvideo/util/CassandraSession.java
+++ webapp/src/main/java/com/datastax/training/killrvideo/util/CassandraSession.java
@@ -2,7 +2,6 @@ package com.datastax.training.killrvideo.util;
 
 import java.util.UUID;
 
-import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.core.utils.UUIDs;
 import com.datastax.driver.mapping.Mapper;
@@ -24,13 +23,6 @@ public class CassandraSession {
             String keyspace, String username,
             String password, String... contactpoints) {
 
-        Cluster cluster = Cluster.builder()
-                .withCredentials(username, password)
-                .addContactPoints(contactpoints)
-                .build();
-        session = cluster.connect(keyspace);
-
-        mappingManager = new MappingManager(session);
     }
 
     public static Session getSession() {
